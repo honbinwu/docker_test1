9b0aacba96bb03e41d1d6ed777219198798184d6
jerry wu <a0938489128@gmail.com>
2020-11-10 18:50:55 +0800

    0.0.1

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+const express 	= require( 'express' )
+const appRouter = express.Router()
+
+appRouter.get( '/*', function( req, res ) {
+  res.send( '<h1>Hello, DockerCon!</h1>' )
+})
+
+module.exports = appRouter
+
